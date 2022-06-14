0aa1dc259b27e1d947e265266bcc55abdaf374d8
Volodymyr Lysenko <tibank2071@gmail.com>
2022-06-14 17:10:57 +0300

    feat: implement user service

--- app.ts
+++ app.ts
@@ -1,4 +1,15 @@
+import http from 'http'
+import 'dotenv/config'
 
+const PORT: string|number = process.env.PORT || 3000;
 
+const app: http.Server = http.createServer((req: http.IncomingMessage, res: http.OutgoingMessage) => {
+    console.log(req.url);
+    console.log(req.method);
+    res.end('done')
+})
 
+app.listen(PORT, (): void => {
+    console.log(`Server running at http://localhost:${PORT}`)
+})
 
--- src/server/server.ts
+++ src/server/server.ts
@@ -2,6 +2,37 @@ import http from 'http'
 import EventEmitter  from 'events'
 
 export class Application {
-    emitter: EventEmitter = new EventEmitter()
+    server: http.Server
+    emitter: EventEmitter
+    middlewares: any []
+
+    constructor() {
+        this.emitter = new EventEmitter()
+        this.server = this._createServer()
+        this.middlewares = []
+    }
+
+    listen(port: string|number, callback: any ):void {
+        this.server.listen(port, callback)
+    }
+
+    use(middleware: any):void {
+        this.middlewares.push(middleware)
+    }
+
+    _createServer(): http.Server {
+        return http.createServer((req: http.IncomingMessage, res: http.OutgoingMessage) => {
+            let body = ""
+
+            req.on('data', (chunk: any) => {
+                body += chunk;
+             })
+
+             req.on('end', () => {
+                console.log(body)
+             })
+
+        })
+    } 
 
 }
